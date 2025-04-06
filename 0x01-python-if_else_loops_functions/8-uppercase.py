47015f1806010f56687d30551898e3cc422e257f
David Idowu <idowudavidodun@gmail.com>
2025-04-06 22:21:56 +0100

    2 print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,8 @@
 def uppercase(str):
     for let in str:
         if ord(let) >= 97 and ord(let) <= 123:
-            print("{}".format(chr(ord(let) - 32)), end="")
+            lower = ord(let) - 32
         else:
-            print("{}".format(let), end="")
+            lower = ord(let)
+        print("{}".format(chr(lower)), end="")
     print()

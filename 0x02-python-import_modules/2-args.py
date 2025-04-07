f5546f7f0e18f423a6ce2bf1b7472193fd6f575e
David Idowu <idowudavidodun@gmail.com>
2025-04-07 22:00:46 +0100

    :

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         if argc == 1:
             print("1 argument:")
         else:
-            print("{} arguments".format(argc))
+            print("{} arguments:".format(argc))
         for i in range(1, argc + 1):
             print("{}: {}".format(i, sys.argv[i]))

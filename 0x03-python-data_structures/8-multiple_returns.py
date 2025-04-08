e62f5af20c8132e839803069b93e662344c6ad11
David Idowu <idowudavidodun@gmail.com>
2025-04-08 23:23:28 +0100

    None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence or sentence == "":
-        sentence[0] = None
-    return (len(sentence), sentence[0])
+        s = None
+    else:
+        s = sentence[0]
+    return (len(sentence), s)

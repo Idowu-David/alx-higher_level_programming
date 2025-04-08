17ee9e2e0c444e3a4b2ccb4d7999184ba111eb16
David Idowu <idowudavidodun@gmail.com>
2025-04-08 23:30:05 +0100

    comprehension[

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new = []
-    for i in my_list:
-        if i % 2 == 0:
-            new.append(True)
-        else:
-            new.append(False)
-    return new
+    return [i % 2 for i in my_list]

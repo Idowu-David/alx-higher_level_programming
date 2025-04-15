74b2a6eaf9433b081284199c958307e6e0832306
David Idowu <idowudavidodun@gmail.com>
2025-04-15 22:18:08 +0100

    square

---
+++ 0x06-python_classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Represents a geometric square.
+    
+    Attributes:
+        size (int): The length of one side of the square.
+    """
+    def __init__(self, size):
+        self.__size = size

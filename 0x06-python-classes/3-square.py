9a98940a3c60c76445b2009c0b9b69820aa7e777
David Idowu <idowudavidodun@gmail.com>
2025-04-15 22:31:14 +0100

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class for basic geometric calculations.add()
+
+Classes:
+    Square: Represents a square
+"""
+
+
+class Square:
+    """
+    Represents a geometric square.
+
+    Attributes:
+        size (int): The length of one side of the square.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """
+        Calculates the area of the square
+        
+        Returns:
+            int: The area of the square
+        """
+        return self.__size ** 2

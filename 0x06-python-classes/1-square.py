e901fd93f1d7c004d81acf99dfebad8ec0216a75
David Idowu <idowudavidodun@gmail.com>
2025-04-15 22:23:28 +0100

    module docs

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class for basic geometric calculations.add()
+
+Classes:
+    Square: Represents a square
+"""
+
+
 class Square:
     """Defines a square Class"""
     def __init__(self, size):
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class for basic geometric calculations.add()
+
+Classes:
+    Square: Represents a square
+"""
+
+
 class Square:
     """
     Represents a geometric square.
-    
+
     Attributes:
         size (int): The length of one side of the square.
     """

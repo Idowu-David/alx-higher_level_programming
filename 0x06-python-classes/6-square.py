1d1732cf05e123eec73541a6d4eed67a7ea1b5d6
David Idowu <idowudavidodun@gmail.com>
2025-04-16 15:05:22 +0100

    square position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class for basic geometric calculations.add()
+
+Classes:
+    Square: Represents a square
+"""
+
+
+class Square:
+    """
+    Represents a geometric square.
+
+    Attributes:
+        size (int): The length of one side of the square.
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Gets the size of the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of size after validations"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Gets the position of the square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position of the square"""
+        if not isinstance(value, tuple) or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """
+        Calculates the area of the square
+
+        Returns:
+            int: The area of the square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the character #"""
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                print(" " * self.position[0], end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print("#")

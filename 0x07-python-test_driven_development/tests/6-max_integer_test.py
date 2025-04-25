7e16d261afef961627cc6991e69406c3ae88a925
David Idowu <idowudavidodun@gmail.com>
2025-04-25 15:37:07 +0100

    tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,20 +16,6 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-10, -20, 0, -40]), 0)
         self.assertEqual(max_integer([1.2, -2.9, 0.3, -4.5]), 1.2)
         self.assertEqual(max_integer([3]), 3)
-
-    def test_input(self):
-        """test that the input is an integer or float"""
-        with self.assertRaises(ValueError):
-            max_integer([1, "2", 4, 5])
-        with self.assertRaises(ValueError):
-            max_integer([1, (3, 4), 5])
-    
-    def test_raises(self):
-        """test that an exception was raised if the argument is not a list"""
-        with self.assertRaises(TypeError):
-            max_integer((3, 4, 5))
-        with self.assertRaises(TypeError):
-            max_integer("3, 4, 5")
       
 if __name__ == '__main__':
     unittest.main()

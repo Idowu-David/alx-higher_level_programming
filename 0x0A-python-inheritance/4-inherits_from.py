15744d53db4b8e5f67a0b9a415f51e849fc946a7
David Idowu <idowudavidodun@gmail.com>
2025-05-07 19:26:53 +0100

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""inherits\_from modul"""
+
+
+def inherits_from(obj, a_class):
+	"""
+	checks if the object is an instance of a class that inherited
+	(directly or indirectly) from the specified class
+	"""
+	return isinstance(obj, a_class) and type(obj) is not a_class

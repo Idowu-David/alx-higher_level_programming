59690a74292255f1021f518282cb90f909ebb53c
David Idowu <idowudavidodun@gmail.com>
2025-05-07 19:57:58 +0100

    area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+"""the `BaseGeometry` module"""
+
+
+class BaseGeometry:
+    """defines the BaseGeometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")

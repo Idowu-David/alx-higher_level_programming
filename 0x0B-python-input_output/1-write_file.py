83854a34c24d92b7278c42ecd2b97402ca55953d
David Idowu <idowudavidodun@gmail.com>
2025-05-09 21:34:34 +0100

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,7 @@
 
 def write_file(filename="", text=""):
     """writes a string to a text file (UTF8)
-    
+
     Return: the number of characters
     """
     with open(filename, 'w', encoding='utf-8') as f:

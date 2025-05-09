5da4ded4634d24ce3993b6273204817813c49acc
David Idowu <idowudavidodun@gmail.com>
2025-05-09 21:52:41 +0100

    to_json_string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""the `to_json_string` module"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """returns the JSON representaion of an object (string)"""
+    return json.dumps(my_obj)

535b9dc609cd0a39bc0106620637fb585a6d3b33
David Idowu <idowudavidodun@gmail.com>
2025-05-09 17:02:15 +0100

    fixed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,25 +3,7 @@
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    """defines a Rectangle class"""
-    def __init__(self, width, height):
-        """initialization"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """calculates the area of the rectangle"""
-        return self.__height * self.__width
-
-    def __str__(self):
-        """returns the string representation of rectangle"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """defines a Square geometry"""
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-MyInt = __import__('100-my_int').MyInt
+Square = __import__('10-square').Square
+Rectangle = __import__('9-rectangle').Rectangle
 
-my_i = MyInt(3)
-print(my_i)
-print(my_i == 3)
-print(my_i != 3)
+print(issubclass(Square, Rectangle))

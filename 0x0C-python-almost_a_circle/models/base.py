fb4473153fda01129134700a9f0c5b5ba6c910c7
David Idowu <idowudavidodun@gmail.com>
2025-05-12 21:52:46 +0100

    doc base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,13 @@
 
 
 class Base:
+    """
+    Base class for managing id attributes across all future classes.
+
+    Attributes:
+        __nb_objects (int): A class-level counter to assign unique IDs.
+        id (int): The identity of the instance, set manually or automatically
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):

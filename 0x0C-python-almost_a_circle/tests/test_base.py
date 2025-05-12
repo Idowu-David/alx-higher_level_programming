f92ee5b940985a50dc4b8fdb1a709139a8057701
David Idowu <idowudavidodun@gmail.com>
2025-05-12 21:32:07 +0100

    fixed docs

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -7,16 +7,26 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    """tests the Base class"""
+    """
+    Unit tests for the Base class.
+
+    This test case verifies the behavior of the Base class, particularly
+    its automatic ID assignment and handling of user-defined IDs
+    """
+
     def test_base_class(self):
-        """tests the base class"""
+        """
+        Test automatic and manual ID assignment in Base instances
+
+        - When no ID is provide, the Base class should auto-increment
+        - When an ID is manually specified, it should override the
+        auto-assigned one.
+        """
         b1 = Base()
-        self.assertAlmostEqual(b1.id, 1)
+        self.assertEqual(b1.id, 1)
         b2 = Base()
         self.assertEqual(b2.id, 2)
         b3 = Base(12)
         self.assertEqual(b3.id, 12)
         b4 = Base()
         self.assertEqual(b4.id, 3)
-
-  
--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -42,6 +42,5 @@ class TestBase(unittest.TestCase):
             Rectangle(3, 4, 3, -2)
 
 
-  
 if __name__ == '__main__':
     unittest.main()

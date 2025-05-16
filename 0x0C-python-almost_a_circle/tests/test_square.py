58571c4659393ba4ee1a786405744662fbd9f87a
David Idowu <idowudavidodun@gmail.com>
2025-05-17 00:50:11 +0100

    tests

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -24,6 +24,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(s3.id, 3)
         s4 = Square(1, 2, 3, 12)
         self.assertEqual(s4.id, 12)
+        s = Square(1)
+        self.assertEqual(s.size, 1)
 
     def test_rectangle_inheritance(self):
         """tests the inheritance of rectangle from base class"""
@@ -43,6 +45,32 @@ class TestBase(unittest.TestCase):
             Square(2, 4, "2", 1)
         with self.assertRaises(ValueError):
             Square(3, 4, -3, 2)
+    
+    def test_size_zero_raises(self):
+        """tests for 0 input"""
+        with self.assertRaises(ValueError) as cm:
+            Square(0)
+        self.assertEqual(str(cm.exception), "width must be > 0")
+
+    def test_str(self):
+        """tests the __str__ method"""
+        s = Square(4, 2, 1, 99)
+        self.assertEqual(str(s), "[Square] (99) 2/1 - 4")
+
+    def test_update_args(self):
+        s = Square(5)
+        s.update(10, 7, 2, 3)
+        self.assertEqual(str(s), "[Square] (10) 2/3 - 7")
+
+    def test_update_kwargs(self):
+        s = Square(5)
+        s.update(id=42, size=10, y=7)
+        self.assertEqual(str(s), "[Square] (42) 0/7 - 10")
+
+    def test_to_dictionary(self):
+        s = Square(10, 2, 1, 1)
+        expected = {'id': 1, 'size': 10, 'x': 2, 'y': 1}
+        self.assertEqual(s.to_dictionary(), expected)
 
 
 if __name__ == '__main__':

f96e7e03ac5961d1ce25ec1eb3f8d4c0f2d104e4
David Idowu <idowudavidodun@gmail.com>
2025-05-17 23:10:42 +0100

    argv fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,11 @@
 const { argv } = require('node:process');
 
 if (argv.length > 2) {
-  console.log('Argument found');
+  if (argv.length === 3) {
+    console.log('Argument found');
+  } else if (argv.length > 3) {
+    console.log('Arguments found');
+  }
 } else {
   console.log('No argument');
 }

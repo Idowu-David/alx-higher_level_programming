4e03bdcd4a4182c536c501e5392a1f84ae4fc971
David Idowu <idowudavidodun@gmail.com>
2025-05-17 23:27:09 +0100

    to integer fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-const { argv } = require("node:process");
+const { argv } = require('node:process');
 
-myNum = argv[2];
+const myNum = argv[2];
 if (myNum === undefined) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
-	const n = Number(myNum);
-	if (n) {
-		console.log('My number: %s', n);
-	} else {
-		console.log('Not a number');
-	}
+  const n = Number(myNum);
+  if (n) {
+    console.log('My number: %s', n);
+  } else {
+    console.log('Not a number');
+  }
 }

713e84f91c4bc218b928183970b30bd19575796c
David Idowu <idowudavidodun@gmail.com>
2025-05-18 12:43:32 +0100

    multi c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require("node:process");
+
+numTimes = Number(argv[2]);
+if (numTimes) {
+  for (let i = 0; i < numTimes; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurences");
+}

fbdaa8e1cb22a38d960bbdfef943989700f18567
David Idowu <idowudavidodun@gmail.com>
2025-05-21 19:52:16 +0100

    isinteger

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) return;
+    if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) return;
     this.width = w;
     this.height = h;
   }

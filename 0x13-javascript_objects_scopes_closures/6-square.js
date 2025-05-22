ca61ff822ba3a50491c02c8a6d9a0d5fc1f81f68
David Idowu <idowudavidodun@gmail.com>
2025-05-22 12:00:27 +0100

    semi

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 
-const Rectangle = require("./5-square");
+const Rectangle = require('./5-square');
 
 class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size);
   }
 
-  charPrint(c) {
-    if (!c) c = "X";
-    let sq = "";
+  charPrint (c) {
+    if (!c) c = 'X';
+    let sq = '';
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         sq += c;
       }
-      if (i < this.height - 1) sq += "\n";
+      if (i < this.height - 1) sq += '\n';
     }
     console.log(sq);
   }

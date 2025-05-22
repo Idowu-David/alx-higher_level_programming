0466af149e83265d19b4f88e3f3e171402551f6f
David Idowu <idowudavidodun@gmail.com>
2025-05-22 11:55:15 +0100

    square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const Rectangle = require("./4-rectangle");
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+    if (!c) c = "X";
+    let sq = "";
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        sq += c;
+      }
+      if (i < this.height - 1) sq += "\n";
+    }
+    console.log(sq);
+  }
+}
+
+module.exports = Square;
